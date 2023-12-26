ebc04172b1c2a98fc6e0482a0b1841b5c527df05
Atanu Paul <atanupaul2411@gmail.com>
2023-12-26 23:21:30 +0530

    rolling the dice app initialized

---
+++ rollingDice/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return <Text>Rolling The Dice</Text>;
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
