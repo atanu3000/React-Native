303d89e7d5b9a4928ec396c3b5b721db46945a29
Atanu Paul <atanupaul2411@gmail.com>
2024-01-04 23:33:49 +0530

    music services and assets added

---
+++ MusicPlayer/src/App.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>Music Player App</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ MusicPlayer/src/Constants.ts
@@ -0,0 +1,67 @@
+import { Track } from "react-native-track-player";
+
+export const playListData : Track[] = [
+    {
+        id: 1,
+        title: 'Maan Meri Jaan',
+        artist: 'King',
+        album: 'Champagne Talk',
+        artwork:
+          'https://c.saavncdn.com/734/Champagne-Talk-Hindi-2022-20221008011951-500x500.jpg',
+        url: require('./assets/audio/Maan Meri Jaan _ Official Music Video _ Champagne Talk _ King(MP3_320K).mp3'),
+      },
+      {
+        id: 2,
+        title: 'Apna Bana Le',
+        artist: 'Sachin-Jigar, Arijit Singh',
+        album: 'Bhediya',
+        artwork:
+          'https://c.saavncdn.com/815/Bhediya-Hindi-2023-20230927155213-500x500.jpg',
+        url: require('./assets/audio/Apna Bana Le - Bhediya _ Varun Dhawan_ Kriti Sanon_ Sachin-Jigar_ Arijit Singh_ Amitabh Bhattacharya(MP3_320K).mp3'),
+      },
+      {
+        id: 3,
+        title: 'Kesariya',
+        artist: 'Arijit Singh, Amitabh Bhattacharya',
+        album: 'Brahmastra',
+        artwork:
+          'https://c.saavncdn.com/191/Kesariya-From-Brahmastra-Hindi-2022-20220717092820-500x500.jpg',
+        url: require('./assets/audio/Kesariya - Brahmāstra _ Ranbir Kapoor _ Alia Bhatt _ Pritam _ Arijit Singh _ Amitabh Bhattacharya(MP3_320K).mp3'),
+      },
+      {
+        id: 4,
+        title: 'Jab Tak',
+        artist: 'Armaan Malik',
+        album: 'M.S. Dhoni - The Untold Story',
+        artwork:
+          'https://c.saavncdn.com/328/Pal-Pal-Dil-Ke-Paas-Hindi-2019-20200420150444-500x500.jpg',
+        url: require('./assets/audio/JAB TAK Full Video _ M.S. DHONI -THE UNTOLD STORY _ Armaan Malik_ Amaal Mallik _Sushant Singh Rajput(MP3_320K).mp3'),
+      },
+      {
+        id: 5,
+        title: 'Rang Jo Lagyo',
+        artist: 'Atif Aslam, Shreya Ghoshal, Sachin-Jigar',
+        album: 'Ramaiya Vastavaiya',
+        artwork:
+          'https://c.saavncdn.com/807/Pathaan-Hindi-2022-20221222104158-500x500.jpg',
+        url: require('./assets/audio/Rang Jo Lagyo _ Atif Aslam _ Shreya Ghoshal _ Ramaiya Vastavaiya _ 2013(MP3_160K).mp3'),
+      },
+      {
+        id: 6,
+        title: 'Tere liye aaya main toh ',
+        artist: 'Arijit Singh; Asees Kaur',
+        album: 'Bolna Mahi Bolna',
+        artwork:
+          'https://i.ytimg.com/vi/O4NQQlMg6qM/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-BIAC6AKKAgwIABABGHIgSignMA8=&rs=AOn4CLDf0N-ac8QqgUK-sl26p-czITXs8Q',
+        url: require('./assets/audio/tere liye aaya main toh tere sang jaana(MP3_320K).mp3'),
+      },
+      {
+        id: 7,
+        title: 'Tu Banja Gali Benaras Ki',
+        artist: 'Asees Kaur',
+        album: 'Shaadi Mein Zaroor Aana',
+        artwork:
+          'https://c.saavncdn.com/861/Shaadi-Mein-Zaroor-Aana-Hindi-2017-20171024101945-500x500.jpg',
+        url: require('./assets/audio/Tu Banja Gali Benaras Ki - Radio Edit _ Asees Kaur _ Shaadi Mein Zaroor Aana _Rajkummar Rao_ Kriti K(MP3_160K).mp3'),
+      },
+];
