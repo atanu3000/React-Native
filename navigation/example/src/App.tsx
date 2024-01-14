14bb27e45ae9c16e01acb618e480ea9f03dca0e0
Atanu Paul <atanupaul2411@gmail.com>
2024-01-14 13:15:42 +0530

    moving entire demo app into example folder

---
+++ navigation/example/src/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+// navigation
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+//screens
+import Home from './screens/Home';
+import Details from './screens/Details';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details: {productId: string};
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function App(): React.JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen 
+          name='Home'
+          component={Home}
+          options={{
+            title: "Trending Products"
+          }}
+          />
+          <Stack.Screen 
+          name='Details'
+          component={Details}
+          options={{
+            title: "Product Details"
+          }}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
